d1025e4c9eeb21810529003fba994942fb4591ce
Luana Faria Lane <luanasfff@gmail.com>
2020-01-07 04:16:49 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,21 @@
-class Student 
+class Student < User 
+  
+  attr_accessor   :first_name , :last_name 
+  
+  
 
-end
+  
+  def initialize
+    @knowledge = []
+  end
+  
+  def learn(knowledge)
+    @knowledge << knowledge
+  end 
+
+  def knowledge
+    @knowledge
+  end 
+    
+  
+end 
