5d824170474a07b5febe3db72c1ec70a86bd4dc9
Luana Faria Lane <luanasfff@gmail.com>
2020-01-07 04:10:17 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,11 +5,10 @@ class Student < User
   
 
   
-  def initialize(knowledge, first_name, last_name)
+  def initialize(first_name, last_name)
     @first_name = first_name
     @last_name = last_name
-    @knowledge = []
-    
+   
     
   end
   
