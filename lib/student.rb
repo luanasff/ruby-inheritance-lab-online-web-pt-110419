bd6e5173626ba369043f1c192192360b0b9c1336
Luana Faria Lane <luanasfff@gmail.com>
2020-01-07 04:05:49 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,23 @@
-class Student 
+class Student < User 
+  
+  attr_accessor   :knowledge , :first_name , :last_name 
+  
+  
 
-end
+  
+  def initialize(knowledge)
+    @knowledge = []
+    
+    
+  end
+  
+  def first_name
+    @first_name
+  end 
+  
+  def knowledge
+    @@knowledge
+  end 
+    
+  
+end 
